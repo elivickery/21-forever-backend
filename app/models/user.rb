55e9cd06bfbb845091719b2bf433aec555510b2b
Joe Alfaro <joealfaro@Joes-MacBook-Air.local>
2017-08-18 11:41:09 -0700

    Change name to title in user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :goals
 
-    validates :username, :email, :password, presence: true
+    validates :username, :email, :password_digest, presence: true
     validates :username, :email, uniqueness: true
-    validates :password, length: { minimum: 8 }
+    validates :password_digest, length: { minimum: 8 }
     validates :email, format: { with: /.+\@.+\..+/, message: "needs to be formatted ___@__.__" }
 end
