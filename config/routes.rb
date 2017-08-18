5f918832d6b0b6a933109c907678dea0c0bef1ca
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-18 16:33:23 -0700

    UPDATE config/routes
    Remove triple nested routes (users-goals-days) and replace with goals-days routes. Add categories-goals nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories,  only: [:show]
+  resources :users,       only: [:show, :create, :update]
   resources :media_items, only: [:show]
-  resources :users,       only: [:show, :create, :update] do 
-    resources :goals,     only: [:show, :create, :update, :index] do
-      resources :days,    only: [:index]
-    end
+  resources :categories,  only: [:index, :show] do
+    resources :goals,     only:[:show, :create, :update, :index]
+  end
+  resources :goals,       only: [:show] do
+    resources :days,      only: [:index]
   end
 end
