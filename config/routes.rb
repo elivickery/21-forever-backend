3e81a3674fef607fae7fb59618a2c18b1fb9468e
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-18 16:51:18 -0700

    UPDATE routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users,       only: [:show, :create, :update]
   resources :media_items, only: [:show]
-  resources :categories,  only: [:index, :show] do
+
+  resources :categories,  only: [:show] do
     resources :goals,     only:[:show, :create, :update, :index]
   end
-  resources :goals,       only: [:show] do
-    resources :days,      only: [:index]
-  end
+
+  get '/categories', to: 'categories#index'
+  get '/goals/:goal_id/days', to: 'days#index'
 end
