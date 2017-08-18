c852a1ed0fb4fd41ff1080f04c9710eb9ecaced9
Joe Alfaro <joealfaro@Joes-MacBook-Air.local>
2017-08-18 14:11:39 -0700

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories,  only: [:show]
+  resources :media_items, only: [:show]
+  resources :users,       only: [:show, :create, :update] do 
+    resources :goals,     only: [:show, :create, :update, :index] do
+      resources :days,    only: [:index]
+    end
+  end
 end
