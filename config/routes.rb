3501b77e56c5fad0ff46f105554f6deed2aba3f0
Nathan <npersall@gmail.com>
2017-08-18 17:58:58 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  resources :users,       only: [:show, :create, :update]
-  resources :media_items, only: [:show]
 
-  resources :categories,  only: [:show] do
-    resources :goals,     only:[:show, :create, :update, :index]
+  namespace :api do
+
+    resources :users, param: :access_token,  only: [:show, :create, :update]
+    resources :media_items, only: [:show]
+
+    resources :categories,  only: [:show] do
+      resources :goals, only:[:show, :create, :update, :index]
+    end
+
+    get '/categories', to: 'categories#index'
+    get '/goals/:goal_id/days', to: 'days#index'
+
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+    get    'verify'  => 'sessions#verify_access_token'
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
 
-  get '/categories', to: 'categories#index'
-  get '/goals/:goal_id/days', to: 'days#index'
 end
