ead12475625750907649e066fb19ba4b5861c019
Eli <eli@Elis-MacBook-Air.local>
2017-08-20 14:21:21 -0700

    Remove [session] from sessionscontroller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      log_in user
+      params[:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'You have logged in successfully'
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
