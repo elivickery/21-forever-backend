64cac040639a00870a6978ac2508c887dfd107e7
Elsha Kwee <elsha.e.k@gmail.com>
2017-08-20 18:19:45 -0700

    Add set user method as before_action in DaysController

--- app/controllers/api/days_controller.rb
+++ app/controllers/api/days_controller.rb
@@ -1,7 +1,9 @@
 module Api
   class DaysController < ApplicationController
+    before_action :set_user
+
     def count
-      @current_day = Day.find_by(status: nil)
+      @current_day = @user.days.find_by(status: nil)
       @current_goal = @current_day.goal
       @days = @current_goal.days
       count = @days.length
@@ -9,13 +11,17 @@ module Api
     end
 
     def update
-      @current_day = Day.find_by(status: nil)
+      @current_day = @user.days.find_by(status: nil)
       @current_day.update(day_params)
     end
 
     private
+    def set_user
+      @user = User.find_by(access_token: params[:access_token])
+    end
+
     def day_params
-      params.require(:day).permit(:status, :goal_id)
+      params.require(:day).permit(:status)
     end
   end
 end
