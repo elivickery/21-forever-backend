0d02bd25c6f27c8f46138f741cb6fccb6c50ec45
Elsha Kwee <elsha.e.k@gmail.com>
2017-08-20 18:21:07 -0700

    Remove destroy method from sessions controller and delete comments

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,40 +1,30 @@
 module Api
   class SessionsController < ApplicationController
 
-    def new
-    end
-    #If user login data  valid is it will return the access_token.
     def create
-      user = User.find_by(email: params[:email].downcase)
-        if user && user.authenticate(params[:password])
-          render json: {
-            found: true,
-            id: user.id
-          }.to_json
-        else
-          render json: {
-            found: false,
-            errors: ['Incorrect e-mail or password']
-          }.to_json
-        end
-    end
-    #Verifies the access_token.
-    def verify_access_token
-      user = User.find_by(access_token: params[:access_token])
-        if user
-          render text: "verified", status: 200
-        else
-          render text: "Token failed verification", status: 422
-        end
-    end
-
-
-    #the _url is needed if redirecting for root. - convention/ rails docs say so
-    # root is set in the routes. right now there are no routes set.
-     def destroy
-        log_out if logged_in?
-        redirect_to root_url
+      @user = User.find_by(email: params[:email].downcase)
+      if @user && @user.authenticate(params[:password])
+        render json: {
+          found: true,
+          id: user.id
+        }.to_json
+      else
+        render json: {
+          found: false,
+          errors: ['Incorrect e-mail or password']
+        }.to_json
       end
+    end
+
+    def verify_access_token
+      @user = User.find_by(access_token: params[:access_token])
+      if @user
+        render text: "verified", status: 200
+      else
+        render text: "Token failed verification", status: 422
+      end
+    end
+
   end
 end
 
