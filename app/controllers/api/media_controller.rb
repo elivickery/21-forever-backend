2bcc8736f86f5d51ba55cc0319cbbe5c7194636b
alphonsotran <alphonsotran@gmail.com>
2017-08-22 10:52:13 -0700

    Add route and change method in media controller.

--- app/controllers/api/media_controller.rb
+++ app/controllers/api/media_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class MediaController < ApplicationController
 
-    def health_gif
+    def health
       response = GiphyAdapter.search(health)
       gifs_sample = @results["data"].map {|gif| gif["images"]["fixed_height"]["url"]}.sample
       render :json gifs_sample.to_json
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
     resources :media_items, only: [:show]
 
     resources :goals,     only:[:create, :update]
-    
+
     get '/categories', to: 'categories#index'
     get '/days/count', to: 'days#count'
     put '/days/edit',   to: 'days#update'
     get '/goals/achieved', to: 'goals#achieved'
     get '/goals/current', to: 'goals#current'
+    get 'media/health', to: 'media#health'
     post   '/login',   to: 'sessions#create'
     delete '/logout',  to: 'sessions#destroy'
     get    '/verify',  to: 'sessions#verify_access_token'
