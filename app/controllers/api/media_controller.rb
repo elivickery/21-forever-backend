6b4c2ea780eeaafb00be6a5111243d62860e98d3
alphonsotran <alphonsotran@gmail.com>
2017-08-22 10:49:30 -0700

    Add giphy adapter helper and controller

---
+++ app/controllers/api/media_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class MediaController < ApplicationController
+
+    def health_gif
+      response = GiphyAdapter.search(health)
+      gifs_sample = @results["data"].map {|gif| gif["images"]["fixed_height"]["url"]}.sample
+      render :json gifs_sample.to_json
+    end
+
+end
---
+++ app/helpers/giphy_adapter.rb
@@ -0,0 +1,18 @@
+module GiphyAdapter
+
+  def self.search
+    require 'uri'
+    require 'net/http'
+
+    url = URI("http://api.giphy.com/v1/gifs/search?&rating=pg&q=financial+motivation&api_key=66488fa7813a472d8f999db142dd4ece&limit=15")
+
+    http = Net::HTTP.new(url.host, url.port)
+
+    request = Net::HTTP::Get.new(url)
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = 'ddadb8a8-50a5-5bcd-8447-943525d278ba'
+
+    response = JSON.parse(http.request(request).body)
+    return response
+    # response["data"].map { |result| result["images"]["fixed_height"]["url"] }
+  end
