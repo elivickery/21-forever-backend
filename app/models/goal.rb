4f89fe75427e9741e40fcb7844830732f96c7a4e
Joe Alfaro <joealfaro@Joes-MacBook-Air.local>
2017-08-22 14:44:04 -0700

    create custom validation for goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,4 +4,14 @@ class Goal < ApplicationRecord
     has_many    :days
 
     validates :title, :category_id, :user_id, presence: true
+    validate only_one_active_goal
+
+private
+    def only_one_active_goal
+        goals = User.find_by(id: self.user_id).goals 
+        if goals.where(archived== false && completed== false) <= 1
+            return true
+        end
+        return false
+    end
 end
