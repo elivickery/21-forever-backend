10e89135c94a41ecc2def536b51abc2535f09456
Elsha Kwee <elsha.e.k@gmail.com>
2017-08-22 22:13:48 -0700

    Replace line 15 where with find_by

--- app/controllers/api/days_controller.rb
+++ app/controllers/api/days_controller.rb
@@ -12,7 +12,7 @@ module Api
     def update
       # was returning more than one goal in line 14. Add .last.
       @current_goal = @user.goals.where(completed: false, archived: false).last
-      @current_day = @current_goal.days.where(status: nil)
+      @current_day = @current_goal.days.find_by(status: nil)
       @current_day.update(status: params[:status])
       status = (@current_goal.days.length === 21)
       @current_goal.update(completed: true, archived: true) if status
